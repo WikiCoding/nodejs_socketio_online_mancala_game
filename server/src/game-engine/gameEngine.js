69a259eaffb87629f33ce3ac1d9ddb0ab688a9aa
WikiCoding <tiagomscastro@gmail.com>
2025-04-04 00:35:40 +0100

    Added some improvements, missing removal of global variables and fix the checkStealValueRule

--- server/src/game-engine/gameEngine.js
+++ server/src/game-engine/gameEngine.js
@@ -15,14 +15,14 @@ const play = (clickedPitIndex, isPlayer1, p1PitsCurrent, p2PitsCurrent, p1Curren
   const valueSelected = isPlayer1 ? p1Pits[clickedPitIndex] : p2Pits[clickedPitIndex];
 
   // TODO: in fact, this is not fully ok because it will replace the pits vector but then it gets updated on top of this creating the bug. Neither should be returning updated things...
-  const { p1UpdatedPits, p2UpdatedPits, currentP1Score, currentP2Score, valueSelectedUpdated } = gameRules.checkStealValueRule(p1Pits, p2Pits, isPlayer1, valueSelected, clickedPitIndex, p1Score, p2Score);
+  // const { p1UpdatedPits, p2UpdatedPits, currentP1Score, currentP2Score, valueSelectedUpdated } = gameRules.checkStealValueRule(p1Pits, p2Pits, isPlayer1, valueSelected, p1Score, p2Score);
 
-  p1Pits = p1UpdatedPits;
-  p2Pits = p2UpdatedPits;
-  p1Score = currentP1Score;
-  p2Score = currentP2Score;
+  // p1Pits = p1UpdatedPits;
+  // p2Pits = p2UpdatedPits;
+  // p1Score = currentP1Score;
+  // p2Score = currentP2Score;
 
-  handleTurn(valueSelectedUpdated, clickedPitIndex, isPlayer1);
+  handleTurn(valueSelected, clickedPitIndex, isPlayer1);
 
   gameRules.checkGameOver(p1Pits, p2Pits) ? gameOver = true : gameOver = false;
 
@@ -30,24 +30,52 @@ const play = (clickedPitIndex, isPlayer1, p1PitsCurrent, p2PitsCurrent, p1Curren
 }
 
 const handleTurn = (valueSelected, clickedPitIndex, isPlayer1) => {
-  gameRules.checkRepeatPlayRule(clickedPitIndex, valueSelected, isPlayer1);
-
   const currentGameStateVector = buildCurrentGameStateVector(p1Pits, p1Score, p2Pits, p2Score);
 
   const convertedPitIndex = convertSelectedPitIndex(clickedPitIndex, isPlayer1);
 
   currentGameStateVector[convertedPitIndex] = 0;
 
+  let lastPitIndex = -1;
+
   for (i = valueSelected, j = convertedPitIndex + 1; i > 0; i--) {
     currentGameStateVector[j]++;
+    if (i === 1) lastPitIndex = j;
     j++;
     if (j === 14) j = 0;
   }
 
-  p1Pits = currentGameStateVector.slice(0, 6).reverse();
-  p2Pits = currentGameStateVector.slice(7, 13)
+  repeatPlay = gameRules.checkRepeatPlayRule(lastPitIndex);
+
+  // const revertedPitIndex = revertSelectedPitIndex(lastPitIndex, isPlayer1);
+
+  // console.log('lastPitIndex', lastPitIndex);
+  // console.log('revertedPitIndex', revertedPitIndex);
+
+  // const result = gameRules.checkStealValueRule(currentGameStateVector.slice(0, 6).toReversed(), currentGameStateVector.slice(7, 13), isPlayer1, revertedPitIndex, currentGameStateVector[6], currentGameStateVector[13]);
+
+  // p1Pits = result.p1UpdatedPits;
+  // p2Pits = result.p2UpdatedPits;
+  // p1Score = result.currentP1Score;
+  // p2Score = result.currentP2Score;
+
+  p1Pits = currentGameStateVector.slice(0, 6).toReversed();
+  p2Pits = currentGameStateVector.slice(7, 13);
   p1Score = currentGameStateVector[6];
   p2Score = currentGameStateVector[13];
+}
+
+const revertSelectedPitIndex = (lastPitIndex, isPlayer1) => {
+  // player1 is like this conversion due to array being reversed
+  if (isPlayer1 && lastPitIndex < 6) {
+    return p1Pits.length - 1 - lastPitIndex;
+  }
+
+  if (!isPlayer1 && lastPitIndex > 6 && lastPitIndex < 13) {
+    return lastPitIndex - 7
+  }
+
+  return -1;
 }
 
 const convertSelectedPitIndex = (clickedPitIndex, isPlayer1) => {
@@ -56,7 +84,7 @@ const convertSelectedPitIndex = (clickedPitIndex, isPlayer1) => {
 }
 
 const buildCurrentGameStateVector = (p1Pits, p1Score, p2Pits, p2Score) => {
-  return [...p1Pits.reverse(), p1Score, ...p2Pits, p2Score];
+  return [...p1Pits.toReversed(), p1Score, ...p2Pits, p2Score];
 }
 
 module.exports = { play };
---
+++ server/tests/gameEgine.test.js
@@ -0,0 +1,107 @@
+const { play } = require('../src/game-engine/gameEngine');
+
+describe('Game Engine', () => {
+  describe('play', () => {
+    test('should correctly handle player 1 turn and repeat play due to landing in score pit', () => {
+      const p1Pits = [4, 4, 4, 4, 4, 4];
+      const p2Pits = [4, 4, 4, 4, 4, 4];
+      const p1Score = 0;
+      const p2Score = 0;
+      const clickedPitIndex = 3;
+      const isPlayer1 = true;
+
+      const result = play(clickedPitIndex, isPlayer1, p1Pits, p2Pits, p1Score, p2Score);
+
+      expect(result.p1UpdatedPits[0]).toBe(5); // Selected pit should be empty
+      expect(result.p1UpdatedPits[1]).toBe(5); // Next pit should have one more
+      expect(result.p1UpdatedPits[2]).toBe(5);
+      expect(result.p1UpdatedPits[3]).toBe(0);
+      expect(result.p1UpdatedPits[4]).toBe(4);
+      expect(result.p1UpdatedPits[5]).toBe(4);
+      expect(result.p2UpdatedPits).toEqual(p2Pits); // Player 2 pits should remain unchanged
+      expect(result.p1Score).toBe(1);
+      expect(result.p2Score).toBe(0);
+      expect(result.repeatPlay).toBe(true);
+      expect(result.gameOver).toBe(false);
+    });
+
+    test('should correctly handle player 2 turn and repeat play due to landing in score pit', () => {
+      const p1Pits = [4, 4, 4, 4, 4, 4];
+      const p2Pits = [4, 4, 4, 4, 4, 4];
+      const p1Score = 0;
+      const p2Score = 0;
+      const clickedPitIndex = 2;
+      const isPlayer1 = false;
+
+      const result = play(clickedPitIndex, isPlayer1, p1Pits, p2Pits, p1Score, p2Score);
+
+      expect(result.p2UpdatedPits[0]).toBe(4); // Selected pit should be empty
+      expect(result.p2UpdatedPits[1]).toBe(4); // Next pit should have one more
+      expect(result.p2UpdatedPits[2]).toBe(0);
+      expect(result.p2UpdatedPits[3]).toBe(5);
+      expect(result.p2UpdatedPits[4]).toBe(5);
+      expect(result.p2UpdatedPits[5]).toBe(5);
+      expect(result.p1UpdatedPits).toEqual(p1Pits);
+      expect(result.p1Score).toBe(0);
+      expect(result.p2Score).toBe(1);
+      expect(result.repeatPlay).toBe(true);
+      expect(result.gameOver).toBe(false);
+    });
+
+    test('should correctly handle player 2 turn and overflow to the next player pit', () => {
+      const p1Pits = [4, 4, 4, 4, 4, 4];
+      const p2Pits = [4, 4, 5, 4, 4, 4];
+      const p1Score = 0;
+      const p2Score = 0;
+      const clickedPitIndex = 2;
+      const isPlayer1 = false;
+
+      const result = play(clickedPitIndex, isPlayer1, p1Pits, p2Pits, p1Score, p2Score);
+
+      expect(result.p1UpdatedPits[0]).toBe(4);
+      expect(result.p1UpdatedPits[1]).toBe(4);
+      expect(result.p1UpdatedPits[2]).toBe(4);
+      expect(result.p1UpdatedPits[3]).toBe(4);
+      expect(result.p1UpdatedPits[4]).toBe(4);
+      expect(result.p1UpdatedPits[5]).toBe(5);
+      expect(result.p2UpdatedPits[0]).toBe(4); // Selected pit should be empty
+      expect(result.p2UpdatedPits[1]).toBe(4); // Next pit should have one more
+      expect(result.p2UpdatedPits[2]).toBe(0);
+      expect(result.p2UpdatedPits[3]).toBe(5);
+      expect(result.p2UpdatedPits[4]).toBe(5);
+      expect(result.p2UpdatedPits[5]).toBe(5);
+      expect(result.p1Score).toBe(0);
+      expect(result.p2Score).toBe(1);
+      expect(result.repeatPlay).toBe(false);
+      expect(result.gameOver).toBe(false);
+    });
+
+    test('should correctly handle player 1 turn and score and steal value from player 2 pit when landing on his pit with value 0', () => {
+      const p1Pits = [0, 4, 4, 4, 4, 4];
+      const p2Pits = [4, 4, 4, 4, 4, 4];
+      const p1Score = 0;
+      const p2Score = 0;
+      const clickedPitIndex = 4;
+      const isPlayer1 = true;
+
+      const result = play(clickedPitIndex, isPlayer1, p1Pits, p2Pits, p1Score, p2Score);
+
+      expect(result.p1UpdatedPits[0]).toBe(0);
+      expect(result.p1UpdatedPits[1]).toBe(5);
+      expect(result.p1UpdatedPits[2]).toBe(5);
+      expect(result.p1UpdatedPits[3]).toBe(5);
+      expect(result.p1UpdatedPits[4]).toBe(0);
+      expect(result.p1UpdatedPits[5]).toBe(4);
+      expect(result.p2UpdatedPits[0]).toBe(0);
+      expect(result.p2UpdatedPits[1]).toBe(4);
+      expect(result.p2UpdatedPits[2]).toBe(4);
+      expect(result.p2UpdatedPits[3]).toBe(4);
+      expect(result.p2UpdatedPits[4]).toBe(4);
+      expect(result.p2UpdatedPits[5]).toBe(4);
+      expect(result.p1Score).toBe(5);
+      expect(result.p2Score).toBe(0);
+      expect(result.repeatPlay).toBe(false);
+      expect(result.gameOver).toBe(false);
+    });
+  });
+});
